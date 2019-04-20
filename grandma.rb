6dde2d1382fba138301ca2b0021d4be4d6c9cc74
Maasa Kono <konomaasa@gmail.com>
2019-04-20 16:39:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,5 @@
-def speak_to_grandma(phrase)
-  if phrase.downcase
-    puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase.upcase
-    puts "NO, NOT SINCE 1938!"
-  else phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-end
+def speak_to_grandma("I LOVE YOU GRANDMA!")
+  
 
 # Write a speak_to_grandma method.
 
