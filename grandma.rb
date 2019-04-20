3dd7efba54ef2793e85f5748209a6dec2a303978
Maasa Kono <konomaasa@gmail.com>
2019-04-20 17:23:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "Hi Nana, how are you?".downcase
+  elsif phrase == "Hi Nana, how are you?"
     return "HUH?! SPEAK UP, SONNY!"
   else phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
