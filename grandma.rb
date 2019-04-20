7aa5448c1245c823ba9ecc7569acb16ac13f0b1f
Maasa Kono <konomaasa@gmail.com>
2019-04-20 17:10:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,10 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == "Hi Nana, how are you?"
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
+    puts "NO, NOT SINCE 1938!"
 end
 
 # Write a speak_to_grandma method.
