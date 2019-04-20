052a5f274bae14773e1575b8ca6fd35fef5e9a93
Maasa Kono <konomaasa@gmail.com>
2019-04-20 16:40:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
-def speak_to_grandma("I LOVE YOU GRANDMA!")
-  if speak_to_grandma.upcase
+def speak_to_grandma(phrase = "I LOVE YOU GRANDMA!")
+  if phrase == "I LOVE YOU GRANDMA!".upcase
     puts "I LOVE YOU TOO PUMPKIN!"
 end
 
